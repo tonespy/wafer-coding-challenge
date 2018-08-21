d4444f46d7226292f331b80705af05ec7867dcb6
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-08-21 15:43:27 +0100

    Country Model Should Contain The Following: * Country:- Each and every country has a name, currency/currencies and language(s) * Currency Model:- Identifying currency in the model * Language Model:- Identifying language information in a model

--- wafer-coding-challenge/Model/Country.swift
+++ wafer-coding-challenge/Model/Country.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+/// Country Model would contain information about three variables:
+///
+///     Name: Name of the country
+///     Currenries: Accepted currencies in the country
+///     Languages: Approved Languages in the country
+struct Country: Codable {
+    
+    let name: String?
+    let currencies: [Currency]?
+    let languages: [Language]?
+}
+
+/// Currency Model would contain information about three variables:
+///
+///     code: 3-digit abreviated letter of the currency
+///     name: Full Name of the currency
+///     symbol: Currencies symbol
+struct Currency: Codable {
+    
+    let code: String?
+    let name: String?
+    let symbol: String?
+}
+
+/// Language Model would contain information about four variables:
+///
+///     code: 3-digit abreviated letter of the currency
+///     name: Full Name of the currency
+///     symbol: Currencies symbol
+struct Language: Codable {
+    
+    let iso639_1: String?
+    let iso639_2: String?
+    let name: String?
+    let nativeName: String?
+}
