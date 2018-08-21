70d9c242c83f5740c2d2a92805586b7ba9ecca1d
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-08-21 15:51:15 +0100

    Country Manager Test Should Fulfill The Following: * Have country json data and country manager mock declared * Initialize country json data in setup * De-Initialize country json data and country manager mock when tearing down
    Test Cases:
    ## testcountry_SuccessfulRequest
    * Test should check if when mock is initialized with successful data is passes and country data is more than 0
    
    ## testCountry_SuccessfulBadData
    * Test should check if when mock is initialized with nil data, it fails

--- wafer-coding-challengeTests/Test Cases/Services/CountryManagerTests.swift
+++ wafer-coding-challengeTests/Test Cases/Services/CountryManagerTests.swift
@@ -7,29 +7,61 @@
 //
 
 import XCTest
+@testable import wafer_coding_challenge
 
 class CountryManagerTests: XCTestCase {
     
+    var countryData: Data?
+    var countryManager: CountryManagerProtocol?
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        countryData = loadStubFromBundle(withName: "all-countries", extension: "json")
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        countryData = nil
+        countryManager = nil
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testcountry_SuccessfulRequest() {
+        XCTAssertNotNil(countryData)
+        XCTAssertNil(countryManager)
+        
+        countryManager = MockCountryManager(requestType: RequestType.successful, mockResponse: countryData)
+        XCTAssertNotNil(countryManager)
+        
+        // When fetch country
+        let expect = XCTestExpectation(description: "country_successful_fetch_callback")
+        
+        countryManager!.fetchCountries { (countries, error) in
+            expect.fulfill()
+            XCTAssertNil(error)
+            XCTAssertNotNil(countries)
+            XCTAssert(countries!.count > 0)
         }
+        
+        wait(for: [expect], timeout: 4)
     }
     
+    func testCountry_SuccessfulBadData() {
+        XCTAssertNotNil(countryData)
+        XCTAssertNil(countryManager)
+        
+        countryManager = MockCountryManager(requestType: RequestType.successful, mockResponse: nil)
+        XCTAssertNotNil(countryManager)
+        
+        // When fetch country with bad data
+        let expect = XCTestExpectation(description: "country_failed_fetch_callback")
+        
+        countryManager!.fetchCountries { (countries, error) in
+            expect.fulfill()
+            XCTAssertNil(countries)
+            XCTAssertNotNil(error)
+            XCTAssertEqual(CountryError.invalidResponse, error!)
+        }
+        
+        wait(for: [expect], timeout: 4)
+    }
 }
