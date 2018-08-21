c02af2b041a4dc4ffc93bc0e36a5677d92a1e0b3
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-08-21 18:36:15 +0100

    * Added Country Controller * Cleanup stater code

---
+++ wafer-coding-challenge/UI/Countries/View Model/CountryViewViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CountryViewViewModel.swift
+//  wafer-coding-challenge
+//
+//  Created by Abubakar Oladeji on 21/08/2018.
+//  Copyright © 2018 Tonespy. All rights reserved.
+//
+
+import Foundation
