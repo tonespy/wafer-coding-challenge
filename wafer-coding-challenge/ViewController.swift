5a7b88e6197689d2364754a95dc6929201b640a6
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-08-21 13:05:45 +0100

    Initial Commit

---
+++ wafer-coding-challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wafer-coding-challenge
+//
+//  Created by Abubakar Oladeji on 21/08/2018.
+//  Copyright © 2018 Tonespy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ wafer-coding-challengeTests/wafer_coding_challengeTests.swift
@@ -0,0 +1,36 @@
+//
+//  wafer_coding_challengeTests.swift
+//  wafer-coding-challengeTests
+//
+//  Created by Abubakar Oladeji on 21/08/2018.
+//  Copyright © 2018 Tonespy. All rights reserved.
+//
+
+import XCTest
+@testable import wafer_coding_challenge
+
+class wafer_coding_challengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
