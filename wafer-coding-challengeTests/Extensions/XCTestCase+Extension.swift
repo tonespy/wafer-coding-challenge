7844f5ba131407f6a64d6955ccadf2e69b189109
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-08-21 15:46:35 +0100

    * Added an xctestcase extension for loading mocked country json

--- wafer-coding-challengeTests/Extensions/XCTestCase+Extension.swift
+++ wafer-coding-challengeTests/Extensions/XCTestCase+Extension.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2018 Tonespy. All rights reserved.
 //
 
-import Foundation
+import XCTest
+
+extension XCTestCase {
+    
+    func loadStubFromBundle(withName name: String, extension: String) -> Data {
+        let bundle = Bundle(for: classForCoder)
+        let url = bundle.url(forResource: name, withExtension: `extension`)
+        
+        return try! Data(contentsOf: url!)
+    }
+    
+}
