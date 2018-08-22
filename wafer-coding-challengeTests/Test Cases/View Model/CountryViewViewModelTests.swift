d20ac3d2a1fd304f386b251a8fa6bd379e595457
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-08-22 07:19:12 +0100

    * Test ViewModel for successful response * Test Viewmodel for loading * Test ViewModel for Error

--- wafer-coding-challengeTests/Test Cases/View Model/CountryViewViewModelTests.swift
+++ wafer-coding-challengeTests/Test Cases/View Model/CountryViewViewModelTests.swift
@@ -7,29 +7,151 @@
 //
 
 import XCTest
+@testable import wafer_coding_challenge
 
 class CountryViewViewModelTests: XCTestCase {
     
+    var countryData: Data?
+    var countryManager: CountryManagerProtocol?
+    var viewModel: CountryViewViewModel?
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        countryData = loadStubFromBundle(withName: "all-countries", extension: "json")
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        countryData = nil
+        countryManager = nil
+        viewModel = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCountry_withSuccessfulData() {
+        XCTAssertNotNil(countryData)
+        XCTAssertNil(countryManager)
+        XCTAssertNil(viewModel)
+        
+        countryManager = MockCountryManager(requestType: RequestType.successful, mockResponse: countryData)
+        XCTAssertNotNil(countryManager)
+        
+        viewModel = CountryViewViewModel(service: countryManager!)
+        XCTAssertNotNil(viewModel)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testCountryViewModel_forTopLoading() {
+        
+        // Given
+        XCTAssertNotNil(countryData)
+        XCTAssertNil(countryManager)
+        XCTAssertNil(viewModel)
+        
+        countryManager = MockCountryManager(requestType: RequestType.successful, mockResponse: countryData)
+        XCTAssertNotNil(countryManager)
+        
+        viewModel = CountryViewViewModel(service: countryManager!)
+        XCTAssertNotNil(viewModel)
+        
+        var topIsLoading = false
+        var topStoppedLoading = false
+        let promise = XCTestExpectation(description: "Top Loading Status")
+        viewModel?.updateTopLoadingStatus = { stat in
+            if stat { topIsLoading = stat }
+            else { topStoppedLoading = stat }
+            promise.fulfill()
         }
+        
+        // When
+        viewModel?.fetchData()
+        XCTAssertTrue(topIsLoading)
+        XCTAssertFalse(topStoppedLoading)
+        
+        wait(for: [promise], timeout: 1.0)
     }
     
+    func testCountryViewModel_forbotomLoadingTrue() {
+        // Given
+        XCTAssertNotNil(countryData)
+        XCTAssertNil(countryManager)
+        XCTAssertNil(viewModel)
+        
+        countryManager = MockCountryManager(requestType: RequestType.successful, mockResponse: countryData)
+        XCTAssertNotNil(countryManager)
+        
+        viewModel = CountryViewViewModel(service: countryManager!)
+        XCTAssertNotNil(viewModel)
+        
+        viewModel?.fetchData()
+        
+        var bottomIsLoading = false
+        let promise = XCTestExpectation(description: "Bottom Loading Status")
+        viewModel?.updateBottomLoadingStatus = { stat in
+            bottomIsLoading = stat
+            promise.fulfill()
+        }
+        
+        // When
+        viewModel?.fetchMore()
+        XCTAssertTrue(bottomIsLoading)
+        
+        wait(for: [promise], timeout: 1.0)
+    }
+    
+    func testCountryViewModel_successfulData() {
+        // Given
+        XCTAssertNotNil(countryData)
+        XCTAssertNil(countryManager)
+        XCTAssertNil(viewModel)
+        
+        countryManager = MockCountryManager(requestType: RequestType.successful, mockResponse: countryData)
+        XCTAssertNotNil(countryManager)
+        
+        viewModel = CountryViewViewModel(service: countryManager!)
+        XCTAssertNotNil(viewModel)
+        
+        var countries = [Country]()
+        let promise = XCTestExpectation(description: "Successfu Fetch Countries")
+        viewModel?.displayFetchedCountries = { ctrs in
+            countries.append(contentsOf: ctrs)
+            promise.fulfill()
+        }
+        
+        // When
+        viewModel?.fetchData()
+        XCTAssertTrue(countries.count > 0)
+    }
+    
+    func testCountryViewModel_failedData() {
+        // Given
+        XCTAssertNotNil(countryData)
+        XCTAssertNil(countryManager)
+        XCTAssertNil(viewModel)
+        
+        countryManager = MockCountryManager(requestType: RequestType.successful, mockResponse: nil)
+        XCTAssertNotNil(countryManager)
+        
+        viewModel = CountryViewViewModel(service: countryManager!)
+        XCTAssertNotNil(viewModel)
+        
+        var countries = [Country]()
+        var message = ""
+        let countryPromise = XCTestExpectation(description: "Failed To Fetch Countries")
+        let messagePromise = XCTestExpectation(description: "Error Message")
+        viewModel?.displayFetchedCountries = { ctrs in
+            countries = ctrs
+            countryPromise.fulfill()
+        }
+        
+        viewModel?.displayFetchError = {msg in
+            message = msg
+            messagePromise.fulfill()
+        }
+        
+        // When
+        viewModel?.fetchData()
+        XCTAssertTrue(countries.count == 0)
+        XCTAssertTrue(message.count > 0)
+        
+        wait(for: [countryPromise, messagePromise], timeout: 4.0)
+    }
 }
