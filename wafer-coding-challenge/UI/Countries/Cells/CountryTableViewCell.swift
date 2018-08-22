91cec928a75d0c0b76c994701388c243de7d6147
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-08-22 07:46:22 +0100

    * Remoeved unused controller

---
+++ wafer-coding-challenge/UI/Countries/Cells/CountryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CountryTableViewCell.swift
+//  wafer-coding-challenge
+//
+//  Created by Abubakar Oladeji on 22/08/2018.
+//  Copyright © 2018 Tonespy. All rights reserved.
+//
+
+import UIKit
+
+class CountryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
