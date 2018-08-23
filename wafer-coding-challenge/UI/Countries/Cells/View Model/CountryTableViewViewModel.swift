fd6194f03bfcba54ec51b1c52a372b21534606e2
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-08-23 16:50:50 +0100

    * Added cell tableview viewmodel

--- wafer-coding-challenge/UI/Countries/Cells/View Model/CountryTableViewViewModel.swift
+++ wafer-coding-challenge/UI/Countries/Cells/View Model/CountryTableViewViewModel.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+class CountryTableViewViewModel {
+    
+    private let country: Country
+    
+    var displayName: ((String)->())?
+    var displayCurrency: ((String)->())?
+    var displayLanguage: ((String)->())?
+    
+    init(withCountry country: Country) {
+        self.country = country
+    }
+    
+    func setupModel() {
+        if let countryName = country.name {
+            displayName?(countryName)
+        } else { displayName?("N/A") }
+        
+        if let currencies = country.currencies, currencies.count > 0, let currencyName = currencies[0].name {
+            displayCurrency?(currencyName)
+        } else { displayCurrency?("N/A") }
+        
+        if let languages = country.languages, languages.count > 0, let languageName = languages[0].name {
+            displayLanguage?(languageName)
+        } else { displayLanguage?("N/A") }
+    }
+}
