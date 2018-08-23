80b0bd03423d008de6ae3d0362e2b174949e3189
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-08-23 16:46:23 +0100

    * Setup controller viewmodel for handling property change

--- wafer-coding-challenge/UI/Countries/Extensions/CountryViewUIViewModel+Extension.swift
+++ wafer-coding-challenge/UI/Countries/Extensions/CountryViewUIViewModel+Extension.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2018 Tonespy. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension CountryViewController {
+    
+    func setupViewModel() {
+        if !isViewLoaded { return }
+        
+        guard let viewModel = viewModel else { return  }
+        
+        viewModel.displayFetchedCountries = { ctrs, fetchCount in
+            if fetchCount == 1 {
+                self.countries = ctrs
+            } else {
+                self.countries += ctrs
+            }
+            self.tableView.reloadData()
+        }
+        
+        viewModel.displayFetchError = { message in
+            self.showAlert("Alert!!!", body: message)
+        }
+        
+        viewModel.updateTopLoadingStatus = { stat in
+            if stat { self.refresher.beginRefreshing() }
+            else { self.refresher.endRefreshing() }
+        }
+        
+        viewModel.updateBottomLoadingStatus = { stat in
+            if stat { self.bottomLoading.startAnimating() }
+            else { self.bottomLoading.stopAnimating() }
+        }
+        
+        viewModel.fetchedAllData = { stat in
+            self.doneFetchingAllData = stat
+        }
+    }
+    
+    @objc func refresh() {
+        guard let viewModel = viewModel else { return }
+        viewModel.fetchData()
+    }
+}
