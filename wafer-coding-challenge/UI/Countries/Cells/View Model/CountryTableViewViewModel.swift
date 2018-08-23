1c8c20dfc10c297c392e2da949cd1f472c56670c
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-08-23 16:38:18 +0100

    * Add file

---
+++ wafer-coding-challenge/UI/Countries/Cells/View Model/CountryTableViewViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CountryTableViewViewModel.swift
+//  wafer-coding-challenge
+//
+//  Created by Abubakar Oladeji on 23/08/2018.
+//  Copyright © 2018 Tonespy. All rights reserved.
+//
+
+import Foundation
